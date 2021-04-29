4df4d216e6368a04dd4c7b77e40c7793fa7fe4b8
xumao1995 <1370029907@qq.com>
2021-04-29 16:19:49 +0800

    新增排队人数mapper

--- src/main/java/com/spdb/ci/mapper/AppointMapper.java
+++ src/main/java/com/spdb/ci/mapper/AppointMapper.java
@@ -3,6 +3,7 @@ package com.spdb.ci.mapper;
 import com.spdb.ci.bean.dto.AppointDto;
 import com.spdb.ci.bean.vo.ReqAppoint;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -11,4 +12,6 @@ public interface AppointMapper {
     Integer addAppoint(ReqAppoint reqAppoint);
 
     List<AppointDto> findAppointListByTel(String tel);
+
+    Integer getQueuePersonal(@Param("branch") String branch,@Param("appointDate") String date, @Param("appointTime") String time);
 }
